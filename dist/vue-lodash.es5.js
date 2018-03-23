ff89cf90aa1db3b4784abeaccf3c34de342692b0
Ewocker <chockerlin@gmail.com>
2018-03-22 17:49:57 -0700

    feat: add user custom name.

--- dist/vue-lodash.es5.js
+++ dist/vue-lodash.es5.js
@@ -3,17 +3,39 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-/* eslint-disable */
-var VueLodash = {
-  install: function install(Vue, _) {
-    Vue._ = _;
-    Object.defineProperties(Vue.prototype, {
-      _: {
-        get: function get() {
-          return _;
-        }
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; } /* eslint-disable */
+
+
+var genericInstall = function genericInstall(Vue) {
+  Vue._ = _lodash2.default;
+  Object.defineProperties(Vue.prototype, {
+    _: {
+      get: function get() {
+        return _lodash2.default;
       }
-    });
+    }
+  });
+};
+
+var VueLodash = {
+  install: function install(Vue, options) {
+    if (options && options.name) {
+      Vue[options.name] = _lodash2.default;
+      Object.defineProperties(Vue.prototype, _defineProperty({}, options.name, {
+        get: function get() {
+          return _lodash2.default;
+        }
+      }));
+    } else {
+      genericInstall(Vue);
+    }
     Vue.mixin({
       mounted: function mounted() {
         // Just tell you that it is mounted
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 /* eslint-disable */
+import lodash from 'lodash'
+
+const genericInstall = (Vue) => {
+  Vue._ = lodash
+  Object.defineProperties(Vue.prototype, {
+    _: { get() { return lodash } }
+  })
+}
+
 const VueLodash = {
-  install (Vue, _) {
-    Vue._ = _
-    Object.defineProperties(Vue.prototype, {
-      _: {
-        get() {
-          return _
-        }
-      }
-    })
+  install (Vue, options) {
+    if (options && options.name) {
+      Vue[options.name] = lodash
+      Object.defineProperties(Vue.prototype, {
+        [options.name]: { get() { return lodash } }
+      })
+    }
+    genericInstall(Vue)
     Vue.mixin({
       mounted() {
         // Just tell you that it is mounted
